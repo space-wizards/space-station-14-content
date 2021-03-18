4c900c9778f761f24c5cf0af491c9f4e8d531eb2
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-03-18 13:32:16 +0100

    Fix step surgery yaml

--- Content.Shared/GameObjects/Components/Body/Surgery/Behaviors/StepSurgery.cs
+++ Content.Shared/GameObjects/Components/Body/Surgery/Behaviors/StepSurgery.cs
@@ -1,16 +1,20 @@
 ﻿#nullable enable
 using Content.Shared.GameObjects.Components.Body.Part;
 using Content.Shared.GameObjects.Components.Body.Surgery.Step;
-using JetBrains.Annotations;
 using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.Manager.Attributes;
 
 namespace Content.Shared.GameObjects.Components.Body.Surgery.Behaviors
 {
     public class StepSurgery : SurgeryBehavior
     {
-        [field: DataField("step")]
-        public SurgeryStepPrototype? Step { get; [UsedImplicitly] private set; }
+        [field: DataField("step")] private string? StepId { get; set; }
+
+        public SurgeryStepPrototype? Step => StepId == null
+            ? null
+            : IoCManager.Resolve<IPrototypeManager>().Index<SurgeryStepPrototype>(StepId);
 
         public override bool Perform(IEntity performer, IBodyPart part)
         {
