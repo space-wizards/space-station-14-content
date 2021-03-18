e6a5a097bdabecf0cae773340a5f95c0ebce5188
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-03-18 13:56:33 +0100

    Add equality operators to SurgeryTag

--- Content.Shared/GameObjects/Components/Body/Surgery/SurgeryTag.cs
+++ Content.Shared/GameObjects/Components/Body/Surgery/SurgeryTag.cs
@@ -1,8 +1,35 @@
-﻿namespace Content.Shared.GameObjects.Components.Body.Surgery
+﻿using System;
+
+namespace Content.Shared.GameObjects.Components.Body.Surgery
 {
-    public readonly struct SurgeryTag
+    public readonly struct SurgeryTag : IEquatable<SurgeryTag>
     {
         public string Id { get; init; }
+
+        public bool Equals(SurgeryTag other)
+        {
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is SurgeryTag other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public static bool operator ==(SurgeryTag left, SurgeryTag right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(SurgeryTag left, SurgeryTag right)
+        {
+            return !left.Equals(right);
+        }
 
         public static implicit operator string(SurgeryTag tag)
         {
